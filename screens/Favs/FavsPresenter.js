d1276c829e3383eefa7999363e60793cef6a6e57
ZGenieZ <327jhl@naver.com>
2021-02-09 20:08:31 +0900

    #8.2 Moving the Cards

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { PanResponder, Dimensions } from "react-native";
+import { PanResponder, Dimensions, Animated } from "react-native";
 import styled from "styled-components/native";
 import { apiImage } from "../../api";
 
@@ -11,12 +11,12 @@ const Container = styled.View`
   align-items: center;
 `;
 
-const Card = styled.View`
-  top: 80px;
-  height: ${HEIGHT / 1.5}px;
-  width: 90%;
-  position: absolute;
-`;
+const styles = {
+  top: 80,
+  height: HEIGHT / 1.5,
+  width: "90%",
+  position: "absolute",
+};
 
 const Poster = styled.Image`
   border-radius: 20px;
@@ -25,18 +25,27 @@ const Poster = styled.Image`
 `;
 
 export default ({ results }) => {
+  const position = new Animated.ValueXY();
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: (evt, gesture) => {
-      console.log(evt, gesture);
+    onPanResponderMove: (evt, { dx, dy }) => {
+      position.setValue({ x: dx, y: dy });
     },
   });
+
   return (
     <Container>
       {results.reverse().map((result) => (
-        <Card key={result.id} {...panResponder.panHandlers}>
+        <Animated.View
+          style={{
+            ...styles,
+            transform: [...position.getTranslateTransform()], //getTranslateTransform() => x와 y에 대해서 css 표현을 얻어내는 함수
+          }}
+          key={result.id}
+          {...panResponder.panHandlers}
+        >
           <Poster source={{ uri: apiImage(result.poster_path) }} />
-        </Card>
+        </Animated.View>
       ))}
     </Container>
   );
